9d0ed183d0e10daba04e1540c115f44817664f40
Aghakarim-dev <hemidzade1010@gmail.com>
2025-02-01 18:38:01 +0400

    1

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from '../login/Login'
+import Register from '../register/Register'
+
+const Home = () => {
+  return (
+  <Login/>
+  )
+}
+
+export default Home
